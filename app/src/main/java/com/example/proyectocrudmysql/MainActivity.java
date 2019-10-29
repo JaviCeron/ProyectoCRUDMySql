97bdbc7098a2dee007039de3d338c564dc009900
JaviCeron <50643061+JaviCeron@users.noreply.github.com>
2019-10-29 15:46:56 -0600

    Cree las clases MySingleton y MantenimientoMySQL

--- app/src/main/java/com/example/proyectocrudmysql/MainActivity.java
+++ app/src/main/java/com/example/proyectocrudmysql/MainActivity.java
@@ -11,15 +11,36 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et_codigo, et_descripcion, et_precio;
+    private Button btn_guardar, btn_consultar1, btn_consultar2, btn_eliminar, btn_actualizar;
+    private TextView tv_resultado;
+
+    boolean inputEt=false;
+    boolean inputEd=false;
+    boolean input1=false;
+    int resultadoInsert=0;
+
+    MantenimientoMySQL manto = new MantenimientoMySQL();
+    boolean estadoGuarda = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.content_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        et_codigo = findViewById(R.id.et_cod);
+        et_descripcion = findViewById(R.id.et_descripcion);
+        et_precio = findViewById(R.id.et_precio);
+        btn_guardar = findViewById(R.id.btn_guardar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -28,7 +49,44 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
+
+
         });
+
+        btn_guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(et_codigo.getText().toString().length()==0){
+                    et_codigo.setError("Campo obligatorio");
+                    inputEt = false;
+                }else {
+                    inputEt=true;
+                }
+                if(et_descripcion.getText().toString().length()==0){
+                    et_descripcion.setError("Campo obligatorio");
+                    inputEd = false;
+                }else {
+                    inputEd=true;
+                }
+                if(et_precio.getText().toString().length()==0){
+                    et_precio.setError("Campo obligatorio");
+                    input1 = false;
+                }else {
+                    input1=true;
+                }
+                if (inputEt && inputEd && input1){
+                   String codigo = et_codigo.getText().toString();
+                   String descripcion = et_descripcion.getText().toString();
+                   String precio = et_precio.getText().toString();
+                   manto.guardar(MainActivity.this, codigo, descripcion, precio);
+                   if (estadoGuarda){
+                       Toast.makeText(MainActivity.this, "Registro almacenado correctamente", Toast.LENGTH_SHORT).show();
+                       limpiarDatos();
+                   }
+                }
+            }
+        });
+
     }
 
     @Override
@@ -51,5 +109,12 @@ public class MainActivity extends AppCompatActivity {
         }
 */
         return super.onOptionsItemSelected(item);
+    }
+
+    public void limpiarDatos(){
+        et_codigo.setText(null);
+        et_descripcion.setText(null);
+        et_precio.setText(null);
+        et_codigo.requestFocus();
     }
 }
